1b5e039e109c4422ebcccf12b1f29057d82d2768
Svante-J-TE <svante.jonsson@elev.ga.ntig.se>
2020-11-24 10:15:59 +0100

    added all messages to database
    Co-Authored-By: Markussim <33492046+Markussim@users.noreply.github.com>
    Co-Authored-By: Linus Romland <romlandlinus@gmail.com>

--- server/main.js
+++ server/main.js
@@ -1,22 +1,21 @@
 //imports
-let express = require('express')
-let app = express()
-let port = process.env.PORT || 3000
-let cors = require('cors')
-let http = require('http').Server(app)
-let io = require('socket.io')(http);
-let User = require('./models/User.js')
-let Message = require('./models/Message.js')
-let dBModule = require('./dbModule.js')
-let fs = require('fs')
-let bodyParser = require("body-parser")
+let express = require("express");
+let app = express();
+let port = process.env.PORT || 3000;
+let cors = require("cors");
+let http = require("http").Server(app);
+let io = require("socket.io")(http);
+let MessageModel = require("./models/Message.js");
+let dBModule = require("./dbModule.js");
+let fs = require("fs");
+let bodyParser = require("body-parser");
 
 //Connect to Mongo
-connectToMongo("LiveMessenger")
+connectToMongo("LiveMessenger");
 
 //Sets and uses depedencies etc.
 const clientDir = __dirname + "/client/";
-app.set('view engine', 'ejs')
+app.set("view engine", "ejs");
 app.use(express.json());
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
@@ -28,20 +27,20 @@ app.use(
 );
 
 //GET ROUTES
-app.get('/', (req, res) => {
-  res.render('pages/index')
-})
+app.get("/", (req, res) => {
+  res.render("pages/index");
+});
 
 //Socket.IO ROUTES
-io.on('connection', socket => {
-  socket.on('msg', (msg) => {
-    console.log(msg)
-    io.emit('msg', msg);
+io.on("connection", (socket) => {
+  socket.on("msg", (msg) => {
+    dBModule.saveToDB(createMessage(msg, "tmp"));
+    io.emit("msg", msg);
   });
 });
 
 http.listen(port, function () {
-  console.log('Server listening on port ' + port);
+  console.log("Server listening on port " + port);
 });
 
 //FUNCTIONS
@@ -53,4 +52,10 @@ function connectToMongo(dbName) {
   }
 }
 
-
+function createMessage(Message, User) {
+  let tmp = new MessageModel({
+    message: Message,
+    user: User,
+  });
+  return tmp;
+}
