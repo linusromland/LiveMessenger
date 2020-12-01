23e27e547cd923143fc310ea06a13c2e99a133a0
linusromland <48645689+linusromland@users.noreply.github.com>
2020-12-01 10:52:48 +0100

    bye

---
+++ server/config/passport.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local'),
+    User = require("../models/User.js");
+
+passport.use(new LocalStrategy({
+    usernameField: 'user[email]',
+    passwordField: 'user[password]',
+}, (email, password, done) => {
+    User.findOne({ email })
+        .then((user) => {
+            if (!user || !user.validatePassword(password)) {
+                return done(null, false, { errors: { 'email or password': 'is invalid' } });
+            }
+            return done(null, user);
+        }).catch(done);
+}));
